e201a2e23f32268fe4fdb562f863c60650b79818
Shaun Newman <newman350z@gmail.com>
2021-04-01 13:10:11 -0700

    kinesis: move thumb alt & ctrl modifiers
    I would like to experiment with making my secondary left, large thumb
    key to be a LT. The first step in this is moving CTRL to a different
    location. I moved it to the END position as I tried the LALT position,
    but typing common vim comamnds (C-u, C-e, C-o) were too difficult.
    
    During this change, I moved LALT to my right thumb cluster so that I
    could actually perform Opt-BS, which I use often. This replaces the
    need for the OSL-D command to delete back a word.

--- keyboards/kinesis/keymaps/scnewma/keymap.c
+++ keyboards/kinesis/keymaps/scnewma/keymap.c
@@ -12,7 +12,6 @@
 //    PRNTSC would be the tap command and CLIPSC would be the hold command.
 #define CLIPSC G(S(C(KC_4)))        // Cmd-Shift-Ctrl-4 -> macOS Screenshot (clipboard)
 #define PRNTSC G(S(KC_4))           // Cmd-Shift-4 -> macOS Screenshot
-#define OPTBS  A(KC_BSPC)           // macOS backspace one word
 
 enum custom_keycodes {
     TMX_PRVW = SAFE_RANGE,
@@ -28,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(_SHIFTED), KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,
                       KC_PIPE, KC_BSLS, KC_LEFT, KC_RGHT,
                         /* thumb cluster */
-			           KC_LGUI,  KC_LALT,
+			            KC_LGUI, OSL(_COMMANDS),
                                  KC_HYPR,
-              KC_BSPC, KC_LCTRL, OSL(_COMMANDS),
+               KC_BSPC, _______, KC_LCTL,
 
         KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK,      DF(_BASE), DF(_CBASE), DF(_QWERTY),
         KC_EQL, KC_RPRN, KC_RCBR, KC_RBRC, KC_ASTR, KC_EXLM,
@@ -39,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_B,   KC_M,    KC_W,    KC_V,    KC_Z,    MO(_SHIFTED),
                 KC_UP,  KC_DOWN, KC_AT,   _______,
                         /* thumb cluster */
-                       KC_RCTL,  KC_RGUI,
-                                 _______,
-              _______, KC_ENTER, KC_SPC
+                        KC_LALT,  KC_RGUI,
+                                  _______,
+               _______, KC_ENTER, KC_SPC
 ),
 
 // Shift Layer for Real Programmer's Dvorak
@@ -53,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,   KC_DQUO, S(KC_Q), S(KC_J), S(KC_K), S(KC_X),
                    KC_PIPE ,KC_HASH ,KC_TRNS ,KC_TRNS,
                         /* thumb cluster */
-			            S(KC_LGUI),  S(KC_LALT),
-                                     KC_HYPR,
-            S(KC_BSPC), S(KC_LCTRL), _______,
+			            S(KC_LGUI), _______,
+                                    KC_HYPR,
+            S(KC_BSPC), _______,    S(KC_LCTL),
 
         KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
         KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_PERC,
@@ -64,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         S(KC_B),   S(KC_M), S(KC_W), S(KC_V), S(KC_Z), KC_TRNS,
                    KC_UP,   KC_DOWN, KC_CIRC, KC_NO,
                         /* thumb cluster */
-                       S(KC_RCTL),  S(KC_RGUI),
-                                    _______,
-              _______, S(KC_ENTER), S(KC_SPC)
+                        S(KC_LALT),  S(KC_RGUI),
+                                     _______,
+               _______, S(KC_ENTER), S(KC_SPC)
 ),
 
 // Real Programmer's Colemak DHm
@@ -79,9 +78,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(_CSFT), KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,
                    KC_PIPE, KC_BSLS, KC_LEFT, KC_RGHT,
                         /* thumb cluster */
-			           KC_LGUI,  KC_LALT,
+			            KC_LGUI, OSL(_COMMANDS),
                                  KC_HYPR,
-              KC_BSPC, KC_LCTRL, OSL(_COMMANDS),
+               KC_BSPC, _______, KC_LCTL,
 
         KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK,   DF(_BASE), DF(_CBASE), DF(_QWERTY),
         KC_EQL, KC_RPRN, KC_RCBR, KC_RBRC, KC_ASTR, KC_EXLM,
@@ -90,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_K,   KC_H,    KC_COMM, KC_DOT,  KC_SLSH, MO(_CSFT),
                 KC_UP,   KC_DOWN, KC_AT,   _______,
                         /* thumb cluster */
-                       KC_RCTL,  KC_RGUI,
+                       KC_LALT,  KC_RGUI,
                                  _______,
               _______, KC_ENTER, KC_SPC
 ),
@@ -104,9 +103,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,   S(KC_Z), S(KC_X), S(KC_C), S(KC_D), S(KC_V),
                    KC_PIPE ,KC_HASH ,KC_TRNS ,KC_TRNS,
                         /* thumb cluster */
-			            S(KC_LGUI),  S(KC_LALT),
-                                     KC_HYPR,
-            S(KC_BSPC), S(KC_LCTRL), _______,
+			            S(KC_LGUI), _______,
+                                    KC_HYPR,
+            S(KC_BSPC), _______,    S(KC_LCTL),
 
         KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
         KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_PERC,
@@ -115,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         S(KC_K),   S(KC_H), KC_LABK, KC_RABK, KC_QUES, KC_TRNS,
                    KC_UP,   KC_DOWN, KC_CIRC, KC_NO,
                         /* thumb cluster */
-                       S(KC_RCTL),  S(KC_RGUI),
+                       S(KC_LALT),  S(KC_RGUI),
                                     _______,
               _______, S(KC_ENTER), S(KC_SPC)
 ),
@@ -162,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,
-        OPTBS, KC_NO, TMX_PRVW, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, TMX_PRVW, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,
                KC_NO, KC_NO,    KC_NO, RESET,
                         /* thumb cluster */
